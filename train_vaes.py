995de99fc57491e6c075be386192f4632b44c47a
Alican Bozkurt <alican@ece.neu.edu>
2020-01-21 20:36:21 -0500

    training script

--- train_vaes.py
+++ train_vaes.py
@@ -1,6 +1,8 @@
 import argparse
+from itertools import combinations_with_replacement
 
 import torch.utils.data
+from torch.utils.data import DataLoader
 from torchvision import transforms
 from tqdm.auto import tqdm
 
@@ -20,36 +22,59 @@ parser.add_argument('--dim-z', type=int, default=50,
                     help='latent dimension (default:50)')
 parser.add_argument('--holdout', action='store_true', default=False,
                     help='holdout 9 from training if True')
+parser.add_argument('--mini', action='store_true', default=False,
+                    help='use MiniMNIST if True')
+parser.add_argument('--spt', type=int, default=10,
+                    help='samples per target')
 parser.add_argument('--beta', type=float, default=1.,
                     help='coefficient of rate term in loss')
 parser.add_argument('--holdout-digit', type=int, default=9,
                     help='holdout digit')
+parser.add_argument('--num-neurons', nargs='+', type=int)
+parser.add_argument('--enc-arch', nargs='+', type=int)
+parser.add_argument('--max-depth', type=int, default=4)
+
 args = parser.parse_args()
+if args.mini and args.holdout:
+    raise ValueError("mini and holdout can't be both true")
 args.cuda = not args.no_cuda and torch.cuda.is_available()
-
 torch.manual_seed(args.seed)
-
 device = "cuda" if args.cuda else "cpu"
-
 kwargs = {'num_workers': 1, 'pin_memory': True} if args.cuda else {}
 
 if __name__ == "__main__":
     test_targets = [args.holdout_digit]
     train_targets = list(range(10))
-    if args.holdout:
-        train_targets.remove(args.holdout_digit)
-    train_loader = torch.utils.data.DataLoader(
-        datasets.HoldoutMNIST('.', targets=train_targets, train=True,
-                              download=True, transform=transforms.ToTensor()),
-        batch_size=128, shuffle=True)
-    test_loader = torch.utils.data.DataLoader(
-        datasets.HoldoutMNIST('.', targets=test_targets, train=False,
-                              download=True, transform=transforms.ToTensor()),
-        batch_size=args.batch_size, shuffle=True)
-    for enc in tqdm([[512, 512, 512]]):
+    if args.mini:
+        train_data = datasets.MiniMNIST('.', samples_per_target=args.spt, train=True,
+                                        download=True, transform=transforms.ToTensor(),
+                                        random=True, seed=args.seed)
+        test_data = datasets.MiniMNIST('.', samples_per_target=args.spt, train=False,
+                                       download=True, transform=transforms.ToTensor(),
+                                       random=True, seed=args.seed)
+    else:
+        if args.holdout:
+            train_targets.remove(args.holdout_digit)
+        train_data = datasets.HoldoutMNIST('.', targets=train_targets, train=True,
+                                           download=True, transform=transforms.ToTensor())
+        test_data = datasets.HoldoutMNIST('.', targets=test_targets, train=False,
+                                          download=True, transform=transforms.ToTensor())
+
+    train_loader = DataLoader(train_data, batch_size=args.batch_size, shuffle=True)
+    test_loader = DataLoader(test_data, batch_size=args.batch_size, shuffle=True)
+    arcs = []
+    for i in range(args.max_depth):
+        arcs.extend([list(a[::-1]) for a in combinations_with_replacement(args.num_neurons, r=i)])
+    for enc in tqdm(arcs):
         vae = VAE(im_shape=[1, 28, 28], dim_z=args.dim_z, encoder=enc, device=device, beta=args.beta)
         vae.fit(train_loader, test_loader, num_epochs=args.epochs)
-        filename = '{prefix}{digit}_z{dim_z}_arch{arch}.tar'.format(prefix='holdout' if args.holdout else 'full',
+        if args.holdout:
+            prefix = 'holdout'
+        elif args.mini:
+            prefix = 'mini'
+        else:
+            prefix = 'full'
+        filename = '{prefix}{digit}_z{dim_z}_arch{arch}.tar'.format(prefix=prefix,
                                                                     digit=args.holdout_digit,
                                                                     dim_z=args.dim_z,
                                                                     arch='_'.join([str(a) for a in enc]))
