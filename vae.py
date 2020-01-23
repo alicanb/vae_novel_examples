bugfix sign in rate and distortion

vae.py
@@ -206,7 +206,7 @@ class ELBOLoss:
 
     @staticmethod
     def rate(mu, log_std, reduction='sum'):
-        rate = 0.5 * (1 + 2 * log_std - mu ** 2 - (2 * log_std).exp())
+        rate = -0.5 * (1 + 2 * log_std - mu ** 2 - (2 * log_std).exp())
         if reduction == 'sum':
             rate = torch.sum(rate)
         elif reduction == 'mean':
@@ -219,12 +219,12 @@ class ELBOLoss:
     def distortion(recon_x, x, reduction='sum'):
         eps = torch.finfo(recon_x.dtype).eps
         recon_x = recon_x.clamp(min=eps, max=1 - eps)
-        return -F.binary_cross_entropy(recon_x, x, reduction=reduction)
+        return F.binary_cross_entropy(recon_x, x, reduction=reduction)
 
     def __call__(self, recon_x, x, mu, log_std):
         rate = self.rate(mu, log_std, reduction=self.reduction)
         distortion = self.distortion(recon_x, x, reduction=self.reduction)
-        return -(distortion + self.beta * rate), rate, distortion
+        return distortion + self.beta * rate, rate, distortion
 
 
 def normal_logprob(z, mu, log_std):